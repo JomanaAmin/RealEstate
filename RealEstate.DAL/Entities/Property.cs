e79a2668b353d059c7c0cf428dd34c7494727f12
JOMANA HELLO <j.hussein2327@nu.edu.eg>
2025-10-23 22:48:38 +0300

    Add project files.

---
+++ RealEstate.DAL/Entities/Property.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstate.DAL.Entities
+{
+    internal class Property
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Category Category { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public decimal Price { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public int Rooms { get; set; }
+        public double AreaSize { get; set; }
+        public bool Furnished { get; set; }
+        public bool IsAvailable { get; set; }
+        public string ContactPhone { get; set; }
+        public string ContactWhatsapp { get; set; }
+
+
+    }
+}
