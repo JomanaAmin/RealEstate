e18e4b77ecc627d2b69734693405fb2e83d6f988
JOMANA HELLO <j.hussein2327@nu.edu.eg>
2025-10-24 01:39:39 +0300

    Added extention method for dependecy injection of the database context and unit of work, added connection string

---
+++ RealEstate.DAL/DataAccessExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RealEstate.DAL.DataContext;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace RealEstate.DAL
+{
+    public static class DataAccessExtension
+    {
+        public static IServiceCollection AddDataAccessLayer(this IServiceCollection service, IConfigurationManager configurationManager) 
+        {
+            service.AddDbContext<RealEstateDataContext>(options => { options.UseSqlServer(configurationManager.GetConnectionString("RealEstateConnection")); });
+            service.AddScoped<Contracts.IUnitOfWork, DataContext.UnitOfWork>();
+            
+            return service;
+        }
+    }
+}
